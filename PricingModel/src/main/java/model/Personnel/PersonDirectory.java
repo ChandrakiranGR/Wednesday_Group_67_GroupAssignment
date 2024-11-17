dedff8f2c3a99877339bbb1c0151857e5e5e4809
sugurushetty.s <sugurushetty.s@northeastern.edu>
2024-11-17 01:08:48 -0500

    MainAreaJPanel

--- PricingModel/src/main/java/model/Personnel/PersonDirectory.java
+++ PricingModel/src/main/java/model/Personnel/PersonDirectory.java
@@ -6,32 +6,31 @@ import java.util.ArrayList;
  * PersonDirectory class - Updated to align with Person constructor
  */
 public class PersonDirectory {
+    
+      ArrayList<Person> personlist ;
+    
+      public PersonDirectory (){
+          
+       personlist = new ArrayList();
 
-    private ArrayList<Person> personList;
-
-    public PersonDirectory() {
-        personList = new ArrayList<>();
     }
 
-    // Updated method to create a new person
-    public Person newPerson(String id, String firstName, String lastName) {
-        Person p = new Person(id, firstName, lastName);
-        personList.add(p);
+    public Person newPerson(String id) {
+
+        Person p = new Person(id);
+        personlist.add(p);
         return p;
     }
 
-    // Find a person by ID
     public Person findPerson(String id) {
-        for (Person p : personList) {
+
+        for (Person p : personlist) {
+
             if (p.isMatch(id)) {
                 return p;
             }
         }
-        return null; // Not found after searching the entire list
-    }
-
-    // Getter for personList (optional)
-    public ArrayList<Person> getPersonList() {
-        return personList;
-    }
+            return null; //not found after going through the whole list
+         }
+    
 }
